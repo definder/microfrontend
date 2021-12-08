d9e5dd3b6748d1c048c9d643971962cd373c1c6f
Nemarsky Eugene <niamarsky@gmail.com>
2021-12-08 16:23:12 +0300

    some chagnes

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import MarketingApp from './components/MarketingApp'
 
-console.log('RUN Marketing APP!')
+console.log('RUN Marketing APP')
 
 export default () => {
   return (
