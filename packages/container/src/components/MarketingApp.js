c6b1134846c4b312f7e5a8753fc4d2292428e251
Nemarsky Eugene <niamarsky@gmail.com>
2021-12-09 12:27:59 +0300

    Create routing communication between conatiner and marketing

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,11 +1,23 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 import { mount } from 'marketing/MarketingApp'
 
 export default () => {
   const ref = React.useRef(null)
+  const history = useHistory()
 
   React.useEffect(() => {
-    mount(ref.current)
+    const { onParentNavigate } = mount(ref.current, {
+      onNavigate: ({ pathname: nextPathname }) => {
+        const { pathname } = history.location
+
+        if (pathname !== nextPathname) {
+          history.push(nextPathname)
+        }
+      },
+    })
+
+    history.listen(onParentNavigate)
   }, [])
 
   return <div ref={ref} />
