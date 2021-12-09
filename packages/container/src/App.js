bda68001f4c01732534525eda582eed20b6b91b1
Nemarsky Eugene <niamarsky@gmail.com>
2021-12-09 17:50:19 +0300

    Add Dashboard

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,5 +1,12 @@
-import React, { lazy, Suspense, useState } from 'react'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import React, { lazy, Suspense, useState, useEffect } from 'react'
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Router,
+  Redirect,
+} from 'react-router-dom'
+import { createBrowserHistory } from 'history'
 import {
   StylesProvider,
   createGenerateClassName,
@@ -9,16 +16,25 @@ import Progress from './components/Progress'
 
 const MarketingLazy = lazy(() => import('./components/MarketingApp'))
 const AuthLazy = lazy(() => import('./components/AuthApp'))
+const DashboardLazy = lazy(() => import('./components/DashboardApp'))
 
 const generateClassName = createGenerateClassName({
   productionPrefix: 'co',
 })
 
+const history = createBrowserHistory()
+
 export default () => {
   const [isSignedIn, setIsSignedIn] = useState(false)
 
+  useEffect(() => {
+    if (isSignedIn === true) {
+      history.push('/dashboard')
+    }
+  }, [isSignedIn])
+
   return (
-    <BrowserRouter>
+    <Router history={history}>
       <StylesProvider generateClassName={generateClassName}>
         <div>
           <Header
@@ -30,11 +46,15 @@ export default () => {
               <Route path="/auth">
                 <AuthLazy onSignIn={() => setIsSignedIn(true)} />
               </Route>
+              <Route path="/dashboard">
+                {!isSignedIn && <Redirect to="/" />}
+                <DashboardLazy />
+              </Route>
               <Route path="/" component={MarketingLazy} />
             </Switch>
           </Suspense>
         </div>
       </StylesProvider>
-    </BrowserRouter>
+    </Router>
   )
 }
---
+++ packages/container/src/components/DashboardApp.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { mount } from 'dashboard/DashboardApp'
+
+export default () => {
+  const ref = React.useRef(null)
+
+  React.useEffect(() => {
+    mount(ref.current)
+  }, [])
+
+  return <div ref={ref} />
+}
